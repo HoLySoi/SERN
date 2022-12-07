16e451cd482f0ce3d31479bda38117eb46a893ac
HoLySoi <45162745+HoLySoi@users.noreply.github.com>
2022-12-07 20:39:08 +0700

    #18

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,12 @@
 import express from "express"
+import homeController from "../controllers/homeController"
 
 let router = express.Router()
 
 let initWebRoutes= (app) => {
-    router.get('/', (req,res) => {
-        return res.send('helooo')
-    })
+    router.get('/', homeController.getHomePage)
+
+
 
     return app.use("/", router)
 }
