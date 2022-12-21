68f333d38a8906a1810fd1c5c48519549d726075
HoLySoi <45162745+HoLySoi@users.noreply.github.com>
2022-12-21 22:26:18 +0700

    #65

---
+++ src/models/markdown.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Markdown extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      //dùng để định danh các mối quan hệ
+      // define association here
+    }
+  }
+  Markdown.init(
+    {
+      contentHTML: DataTypes.TEXT("long"),
+      contentMarkdown: DataTypes.TEXT("long"),
+      description: DataTypes.TEXT("long"),
+      doctorId: DataTypes.INTEGER,
+      specialtyId: DataTypes.INTEGER,
+      clinicId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Markdown",
+    }
+  );
+  return Markdown;
+};
