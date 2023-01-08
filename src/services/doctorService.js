1409b091485fee8bd3b11ea9839b74db57ef4b2c
HoLySoi <45162745+HoLySoi@users.noreply.github.com>
2023-01-08 13:43:40 +0700

    #99

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -461,6 +461,11 @@ let getListPatientForDoctor = (doctorId, date) => {
                 },
               ],
             },
+            {
+              model: db.Allcode,
+              as: "timeTypeDataPatient",
+              attributes: ["valueEn", "valueVi"],
+            },
           ],
           raw: false,
           nest: true,
